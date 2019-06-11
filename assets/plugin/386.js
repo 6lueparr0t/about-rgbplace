55c3b25a655a63161556fb0f18e7eacb86bc2535
Daehyun Im <scallop99@godo.co.kr>
2019-06-11 15:44:03 +0900

    auto commit 2019-06-11 09:32:08

--- assets/plugin/386.js
+++ assets/plugin/386.js
@@ -40,77 +40,27 @@ self._386 = self._386 || {
       onePass = _386.onePass,
       speedFactor = 1 / (_386.speedFactor || 1) * 165000;
       wrap = document.createElement('div'),
-      bar = wrap.appendChild(document.createElement('div')),
-
-      cursor = document.createElement('div'),
-      // If the user specified that the visibility is hidden, then we
-      // start at the first pass ... otherwise we just do the 
-      // cursor fly-by
-      pass = (getComputedStyle(document.body)['visibility'] === 'visible') ? 1 : 0,
-      height = window.innerHeight,
-      width = window.innerWidth,
-
-      // this makes the loading of the screen proportional to the real-estate of the window.
-      // it helps keep the cool sequence there while not making it waste too much time.
-      rounds = (height * width / speedFactor),
-      column = width, row = height - character.height;
       
     wrap.id = "wrap386";
-    bar.id = "bar386";
-    cursor.id = "cursor386";
 
-    cursor.innerHTML = bar.innerHTML = '█';
+    document.body.appendChild(wrap);
+    document.body.style.visibility='visible';
 
-    // only inject the wrap if the pass is 0
-    if(pass === 0) {
-      document.body.appendChild(wrap);
-      document.body.style.visibility='visible';
-    } else {
-      document.body.appendChild(cursor);
-      rounds /= 2;
-      character.height *= 4;
-    }
-
+    var curtainDown  = 1;
+    wrap.style.height = '100%';
     var ival = setInterval(function(){
-      for(var m = 0; m < rounds; m++) {
-        column -= character.width;
+        wrap.style.height = 100 - (curtainDown*10) + '%';
+        curtainDown++;
 
-        if(column <= 0) {
-          column = width;
-          row -= character.height;
-        }
-        if(row <= 0) {
-          pass++;
-          row = height - character.height;
-
-          if(pass == 2) {
-            document.body.removeChild(cursor);
+        if(curtainDown*10 > 100) {
             clearInterval(ival);
-          } else {
-            wrap.parentNode.removeChild(wrap);
-            if(onePass) {
-              clearInterval(ival);
-            } else {
-              document.body.appendChild(cursor);
-              rounds /= 2;
-              character.height *= 4;
-            }
-          }
+            //console.log('done');
         }
-
-        if(pass === 0) {
-          bar.style.width = column + "px";
-          wrap.style.height = row + "px";
-        } else {
-          cursor.style.right = column + "px";
-          cursor.style.bottom = row + "px";
-        }
-      }
-    }, 1);
+    }, 100);
   }
 
   window.addEventListener("load", function (event) {
-      //loading();
+      loading();
   });
 }();
 
