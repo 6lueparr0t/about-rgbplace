fb9cfb611b699453a21faf4d8b787eec27c23889
임대현 <daihyun99@gmail.com>
2019-06-22 17:29:33 +0900

    auto commit 2019-06-19 21:53:40

--- assets/js/me.js
+++ assets/js/me.js
@@ -26,35 +26,37 @@ function playSound (time) {
 	}, time);
 }
 
-var typed = new Typed('.text', {
-	strings: ['Name : 임대현^1000\n\nBirth : 1991 / 12 / 30^1000\n\nGender : 남자^1000\n\nJob : 풀 스택을 지향하는 웹(PHP) 개발자'],
-	startDelay: 2000,
-	backSpeed: 100,
-	typeSpeed: 100,
-	preStringTyped :function(array, self){
-		playSound();
-	},
-	onTypingPaused : function(array, self) {
-		clearInterval(interval);
-		//cnt = cnt + 1;
-	},
-	onTypingResumed : function(array, self) {
-		playSound();
-		//if(cnt > 5) {
+window.addEventListener("load", function (event) {
+	var typed = new Typed('.text', {
+		strings: ['Name : 임대현^1000\n\nBirth : 1991 / 12 / 30^1000\n\nGender : 남자^1000\n\nJob : 풀 스택을 지향하는 웹(PHP) 개발자'],
+		startDelay: 2000,
+		backSpeed: 100,
+		typeSpeed: 100,
+		preStringTyped :function(array, self){
+			playSound();
+		},
+		onTypingPaused : function(array, self) {
+			clearInterval(interval);
+			//cnt = cnt + 1;
+		},
+		onTypingResumed : function(array, self) {
+			playSound();
+			//if(cnt > 5) {
 			//document.querySelector('.text').style.position = 'relative';
 			//document.querySelector('.text').style.top = -1 * (60 * (cnt-5)) + 'px';
 			//document.querySelector('.typed-cursor').style.position = 'relative';
 			//document.querySelector('.typed-cursor').style.top = -1 * (60 * (cnt-5)) + 'px';
-		//}
-	},
-	onStringTyped : function(array, self){
-		//playSound(1200);
-		clearInterval(interval);
-	},
-	onComplete: function(array, self){
-		setTimeout(function () {
+			//}
+		},
+		onStringTyped : function(array, self){
+			//playSound(1200);
 			clearInterval(interval);
-			keystrokeSound.pause();
-		}, 100);
-	}
+		},
+		onComplete: function(array, self){
+			setTimeout(function () {
+				clearInterval(interval);
+				keystrokeSound.pause();
+			}, 100);
+		}
+	});
 });
